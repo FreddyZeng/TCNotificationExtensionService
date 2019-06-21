fc6ba38e84e23db381cdd3da568ff1d39ba01058
fanrong <fanrong@addcn.com>
2019-06-21 16:40:34 +0800

    公开swift库函数

--- TCNotificationExtensionService/Classes/TCDataStatisticalNotificationService.swift
+++ TCNotificationExtensionService/Classes/TCDataStatisticalNotificationService.swift
@@ -11,12 +11,12 @@ import Alamofire
 import AdSupport
 import JPushExtension
 
-class TCDataStatisticalNotificationService: UNNotificationServiceExtension {
+open class TCDataStatisticalNotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
     var bestAttemptContent: UNMutableNotificationContent?
 
-    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+    open  override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
         
@@ -96,7 +96,7 @@ class TCDataStatisticalNotificationService: UNNotificationServiceExtension {
 //        }).resume()
     }
     
-    override func serviceExtensionTimeWillExpire() {
+   open  override func serviceExtensionTimeWillExpire() {
         // Called just before the extension will be terminated by the system.
         // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
         if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
